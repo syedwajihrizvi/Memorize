5a0e3b83b6837e97cf9c1d590b53cda8844ac814
Wajih Rizvi <swh2rizv@edu.uwaterloo.ca>
2022-08-18 17:07:58 -0400

    Initial Commit

---
+++ MemorizerGame/MemorizerGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizerGameApp.swift
+//  MemorizerGame
+//
+//  Created by Wajih Rizvi on 2022-08-18.
+//
+
+import SwiftUI
+
+@main
+struct MemorizerGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
